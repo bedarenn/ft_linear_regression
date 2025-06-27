3423c9f4b5cecab130cc383aa27f1c972c54641a
bedarenn <kelk1dus.game@gmail.com>
2025-06-27 08:01:54 +0200

    fix alpha

--- sample/model_io.py
+++ sample/model_io.py
@@ -1,26 +1,38 @@
 import json
 
 
-def save(theta0: float, theta1: float) -> None:
+def save(theta0: float | int, theta1: float | int,
+         alpha: float | int,
+         km_min: float | int, km_max: float | int
+         ) -> None:
     """
     Save coefficient in a JSON file.
     """
 
-    assert isinstance(theta0, float), \
+    assert isinstance(theta0, float | int), \
         f"save :WrongType: theta0 = {type(theta0)}"
-    assert isinstance(theta1, float), \
+    assert isinstance(theta1, float | int), \
         f"save :WrongType: theta1 = {type(theta1)}"
+    assert isinstance(alpha, float | int), \
+        f"save :WrongType: alpha = {type(alpha)}"
+    assert isinstance(km_min, float | int), \
+        f"save :WrongType: km_min = {type(km_min)}"
+    assert isinstance(km_max, float | int), \
+        f"save :WrongType: km_max = {type(km_max)}"
 
     data = {
         "theta0": theta0,
-        "theta1": theta1
+        "theta1": theta1,
+        "alpha": alpha,
+        "km_min": km_min,
+        "km_max": km_max
     }
 
     with open("../json/model.json", "w") as f:
         json.dump(data, f)
 
 
-def load() -> tuple[float, float]:
+def load() -> tuple[float | int, float | int, float | int, float | int, float | int]:
     """
     Load coefficient from a JSON file.
     """
@@ -31,7 +43,12 @@ def load() -> tuple[float, float]:
 
         theta0 = data["theta0"]
         theta1 = data["theta1"]
+        alpha = data["alpha"]
+        km_min = data["km_min"]
+        km_max = data["km_max"]
 
-        return (float(theta0), float(theta1))
+        return (float(theta0), float(theta1),
+                float(alpha),
+                float(km_min), float(km_max))
     except FileNotFoundError:
-        return (0.0, 0.0)
+        return (0.0, 0.0, 0.0, 0.0, 0.0)
--- sample/train.py
+++ sample/train.py
@@ -4,20 +4,33 @@ from model_io import save
 from model_io import load
 
 
-def update(theta0: float, theta1: float,
-           alpha: float, df: pd.DataFrame
+def load_json(df: pd.DataFrame) -> tuple[float | int, float | int, float | int, float | int, float | int]:
+    theta0, theta1, alpha, km_min, km_max = load()
+
+    if (alpha == 0.0):
+        alpha = 0.5
+
+    if (km_min == km_max):
+        km_min = df['km'].min()
+        km_max = df['km'].max()
+
+    return (theta0, theta1, alpha, km_min, km_max)
+
+
+def update(theta0: float | int, theta1: float | int,
+           alpha: float | int, df: pd.DataFrame
            ) -> tuple[float, float]:
     """
     Update the terms of an affine function.
     """
 
-    assert isinstance(theta0, float), \
+    assert isinstance(theta0, float | int), \
         f"update :WrongType: theta0_b = {type(theta0)}"
-    assert isinstance(theta1, float), \
+    assert isinstance(theta1, float | int), \
         f"update :WrongType: theta1_b = {type(theta1)}"
     assert isinstance(df, pd.DataFrame), \
         f"update :WrongType: tab = {type(df)}"
-    assert isinstance(alpha, float), \
+    assert isinstance(alpha, float | int), \
         f"update :WrongType: alpha = {type(alpha)}"
 
     grad0 = 0
@@ -35,7 +48,33 @@ def update(theta0: float, theta1: float,
     return (theta0, theta1)
 
 
-def plot(df: pd.DataFrame, theta0: float, theta1: float) -> None:
+def train_loop(file: str) -> tuple[float | int, float | int]:
+    df = pd.read_csv(file)
+
+    theta0, theta1, alpha, km_min, km_max = load_json(df)
+    df['km'] = (df['km'] - km_min) / (km_max - km_min)
+
+    i = 0
+
+    theta0_old, theta1_old = theta0, theta1
+    theta0, theta1 = update(theta0_old, theta1_old, alpha, df)
+    while ((theta0, theta1) != (theta0_old, theta1_old)):
+        i += 1
+        theta0_old, theta1_old = theta0, theta1
+        theta0, theta1 = update(theta0_old, theta1_old, alpha, df)
+
+    print(f"Model train {i} times with a total of {len(df) * i} values.")
+    save(theta0, theta1, alpha, km_min, km_max)
+
+    theta1_r = theta1 / (km_max - km_min)
+    theta0_r = theta0 - theta1_r * km_min
+
+    return (theta0_r, theta1_r)
+
+
+def plot(df: pd.DataFrame, theta0: float | int, theta1: float | int) -> None:
+    print(theta0, theta1)
+
     x_val = [0, -theta0 / theta1]
     y_val = [theta0, 0]
 
@@ -55,25 +94,9 @@ def plot(df: pd.DataFrame, theta0: float, theta1: float) -> None:
 
 
 def main():
-    df_norm = pd.read_csv("../data/data.csv")
-
-    km_min = df_norm['km'].min()
-    km_max = df_norm['km'].max()
-    df_norm['km'] = (df_norm['km'] - km_min) / (km_max - km_min)
-
-    theta0, theta1 = 0, 0
-    for i in range(0, 2000):
-        theta0_old, theta1_old = load()
-        theta0, theta1 = update(theta0_old, theta1_old, 0.5, df_norm)
-        save(theta0, theta1)
-        if (theta0_old == theta0 and theta1_old == theta1):
-            print(f"Model train {i} times.")
-            break
-
+    theta0, theta1 = train_loop("../data/data.csv")
     df = pd.read_csv("../data/data.csv")
-    theta1_r = theta1 / (km_max - km_min)
-    theta0_r = theta0 - theta1_r * km_min
-    plot(df, theta0_r, theta1_r)
+    plot(df, theta0, theta1)
 
 
 if __name__ == "__main__":
